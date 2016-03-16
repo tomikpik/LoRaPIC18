4757e9f8c0c7b0a7561dc5d11ed1fffa63b5ffbe
pikous <tomas.pikous@gmail.com>
2016-03-16 12:42:00 +0100

    various tweaks

--- main.c
+++ main.c
@@ -113,7 +113,7 @@ void main(void) {
     getUUID();
      
     srand((int)strtol(uuid, NULL, 0));
-    int delay = 1000+(rand()%1000);
+    int delay = 2000+(rand()%1000);
     printf("radio set wdt %d\r\n",delay);
     read_line(); 
     
@@ -165,8 +165,8 @@ void main(void) {
             if(uartState==4){               
                 
                 if(process_message()){      
-                    delay = 1000+(rand()%1000);
-                    wait_1ms((rand()%40)+15);
+                    delay = 2000+(rand()%1000);
+                    wait_1ms((rand()%40)+25);
                     printf("radio tx 42%s%02X%02X0001\r\n",uuid,inc++,val); 
                     read_line(); 
                     read_line();                 
@@ -183,7 +183,7 @@ void main(void) {
             }
             
             if(uartState==5){
-                delay = 1000+(rand()%1000);
+                delay = 2000+(rand()%1000);
                 printf("radio tx 42%s%02X%02X0000\r\n",uuid,inc++,val); 
                 read_line(); 
                 read_line(); 
